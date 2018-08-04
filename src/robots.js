cee6a7077df210caa5d26df678a015398cabaa8d
Kamil Rybiński <kamil.rybinski@quantumlab.co>
2018-08-04 18:28:19 +0200

    Added SearchBox and editorconfig

--- src/robots.js
+++ src/robots.js
@@ -1,44 +1,50 @@
 export const robots = [
-    {
-        id: 1,
-        name: "Joana Dach",
-        username: "Dachowiec",
-        email: "joana.dach@gmail.com"
-    },
-    {
-        id: 2,
-        name: "Jose Crooks",
-        username: "Crooks",
-        email: "jose.crooks@gmail.com"
-    },
-    {
-        id: 3,
-        name: "Miss Name Hegmann",
-        username: "Missy",
-        email: "missy.name@gmail.com"
-    },
-    {
-        id: 4,
-        name: "Kole Stokes",
-        username: "Coca-Cola",
-        email: "coca.cola@gmail.com"
-    },
-    {
-        id: 5,
-        name: "Mario Casper",
-        username: "Marioso",
-        email: "marioso.casper@gmail.com"
-    },
-    {
-        id: 6,
-        name: "Makenzie Anderson",
-        username: "Andersoni",
-        email: "makenzie.anderson@gmail.com"
-    },
-    {
-        id: 7,
-        name: "Mr. Rosanna Durgan",
-        username: "Rossarita D",
-        email: "rossarita.durgan@gmail.com"
-    },
+  {
+    id: 1,
+    name: "Joana Dach",
+    username: "dach.j",
+    email: "joana.dach@gmail.com"
+  },
+  {
+    id: 2,
+    name: "Jose Crooks",
+    username: "crooks.jose",
+    email: "jose.crooks@gmail.com"
+  },
+  {
+    id: 3,
+    name: "Miss Name Hegmann",
+    username: "missy_name",
+    email: "missy.name@gmail.com"
+  },
+  {
+    id: 4,
+    name: "Kole Stokes",
+    username: "coca_cola",
+    email: "coca.cola@gmail.com"
+  },
+  {
+    id: 5,
+    name: "Mario Casper",
+    username: "marioso",
+    email: "marioso.casper@gmail.com"
+  },
+  {
+    id: 6,
+    name: "Makenzie Anderson",
+    username: "andersoni",
+    email: "makenzie.anderson@gmail.com"
+  },
+  {
+    id: 7,
+    name: "Mr. Rosanna Durgan",
+    username: "rossarita_d",
+    email: "rossarita.durgan@gmail.com"
+  },
+  {
+    id: 8,
+    name: "Esperanza Lueilwitz",
+    username: "esperanza.l",
+    email: "esperanza.lue@gmail.com"
+  },
 ];
