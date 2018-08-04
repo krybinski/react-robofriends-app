e9d144c89f577de7d5f04a8081b770864f8f7b73
Kamil Rybiński <kamil.rybinski@quantumlab.co>
2018-08-04 16:13:53 +0200

    Initial project view

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import CardList from "./CardList";
+import { robots } from './robots';
 
-class App extends Component {
-  render() {
+const App = () => {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <div>
+            <h1>RoboFriends</h1>
+            <CardList robots={robots} />
+        </div>
     );
-  }
 }
 
 export default App;
---
+++ src/robots.js
@@ -0,0 +1,44 @@
+export const robots = [
+    {
+        id: 1,
+        name: "Joana Dach",
+        username: "Dachowiec",
+        email: "joana.dach@gmail.com"
+    },
+    {
+        id: 2,
+        name: "Jose Crooks",
+        username: "Crooks",
+        email: "jose.crooks@gmail.com"
+    },
+    {
+        id: 3,
+        name: "Miss Name Hegmann",
+        username: "Missy",
+        email: "missy.name@gmail.com"
+    },
+    {
+        id: 4,
+        name: "Kole Stokes",
+        username: "Coca-Cola",
+        email: "coca.cola@gmail.com"
+    },
+    {
+        id: 5,
+        name: "Mario Casper",
+        username: "Marioso",
+        email: "marioso.casper@gmail.com"
+    },
+    {
+        id: 6,
+        name: "Makenzie Anderson",
+        username: "Andersoni",
+        email: "makenzie.anderson@gmail.com"
+    },
+    {
+        id: 7,
+        name: "Mr. Rosanna Durgan",
+        username: "Rossarita D",
+        email: "rossarita.durgan@gmail.com"
+    },
+];
